8d771c2bc03b32fb3e34c9e9f58b1a575f9b6ac5
Rodrigo Pino <rodrigo.pino@tecsystems.cl>
2022-09-28 11:23:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
 $time = date('Y-m-d\TH:i:s\Z');
 $url = 'https://intcomex-test.apigee.net/v1/getcatalog?locale=es';
 
-//$url = 'https://intcomex-test.apigee.net/v1/getproduct?ku=AC004HUA65';
+$url = 'https://intcomex-test.apigee.net/v1/getproduct?sku=AC004HUA65';
 $apikey = '785cdb69-3197-4870-b028-f4ade6ca501f';
 $accesKey = 'ab672112-d70a-4248-98ab-c5d16dea8c60';
 $signature = hash('sha256',$apikey.','.$accesKey.','.date('Y-m-d\TH:i:s\Z'));
